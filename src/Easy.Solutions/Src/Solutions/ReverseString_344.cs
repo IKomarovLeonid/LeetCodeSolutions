af1ebff2a38cb7b7cc6699e6c4f2b971f6e852a9
Leonid Komarov <Leokom96@gmail.com>
2022-04-19 21:50:13 +0200

    implement 344 string reverse

--- src/Easy.Solutions/Src/Solutions/ReverseString_344.cs
+++ src/Easy.Solutions/Src/Solutions/ReverseString_344.cs
@@ -1,7 +1,30 @@
 ﻿using System;
 namespace Easy.Solutions.Src.Solutions
 {
-    public struct ReverseString_344
+    class ReverseString_344
     {
+        /*
+         * Write a function that reverses a string. The input string is given as an array of characters s.
+
+           You must do this by modifying the input array in-place with O(1) extra memory.
+         */
+
+
+        public void ReverseString(char[] s)
+        {
+            int index = 0;
+            for (int j = s.Length - 1; j >= 0; j--)
+            {
+                if (index <= j)
+                {
+                    var temp = s[j];
+                    s[j] = s[index];
+                    s[index] = temp;
+                    index++;
+                    continue;
+                }
+                break;
+            }
+        }
     }
 }
