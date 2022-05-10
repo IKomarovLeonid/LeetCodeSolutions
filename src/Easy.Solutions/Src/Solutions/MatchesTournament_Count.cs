32ca4170f023ed7d7e6dd08154ab0bb352c671fd
Leonid Komarov <Leokom96@gmail.com>
2022-05-10 22:13:56 +0200

    Add 1688 solution

---
+++ src/Easy.Solutions/Src/Solutions/MatchesTournament_Count.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Easy.Solutions.Src.Solutions
+{
+    public class MatchesTournament_Count
+    {
+        /*
+         * You are given an integer n, the number of teams in a tournament that has strange rules:
+         * If the current number of teams is even, each team gets paired with another team.
+         * A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+         * If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired.
+         * A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+         * Return the number of matches played in the tournament until a winner is decided.
+         * 
+         * 
+         * 
+         */
+
+
+        public int NumberOfMatches(int n)
+        {
+            if (n == 1 || n == 0) return 0;
+            if (n == 2) return 1;
+
+            if (n % 2 == 0) return n / 2 + NumberOfMatches(n / 2);
+
+            return 1 + NumberOfMatches(--n);
+        }
+    }
+}
