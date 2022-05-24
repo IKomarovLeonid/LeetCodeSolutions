b269586605f083a51f36c1c1cffc83a564273fbb
Leonid Komarov <Leokom96@gmail.com>
2022-05-24 22:12:53 +0200

    Add 2278

---
+++ src/Easy.Solutions/Src/Solutions/PercentageOfLetter_2278.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Easy.Solutions.Src.Solutions
+{
+    public class PercentageOfLetter_2278
+    {
+        public int PercentageLetter(string s, char letter)
+        {
+            var count = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == letter) count++;
+            }
+            return count * 100 / s.Length;
+        }
+    }
+}
