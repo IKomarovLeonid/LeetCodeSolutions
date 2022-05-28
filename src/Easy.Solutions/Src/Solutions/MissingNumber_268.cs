2ac8b4e62f5cd5b610185b5c22280450b7d563c2
leonid <leokom96@gmail.com>
2022-05-28 12:21:19 +0200

    Add 268

---
+++ src/Easy.Solutions/Src/Solutions/MissingNumber_268.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace Easy.Solutions.Solutions
+{
+    public class MissingNumber_268
+    {
+        public int MissingNumber(int[] array)
+        {
+            var rez = -1;
+            var ht = new Hashtable();
+            foreach (var t in array)
+            {
+                ht.Add(t, t);
+            }
+            Console.WriteLine();
+            var j = 0;
+            while (j < array.Length)
+            {
+                if (!ht.ContainsKey(j))
+                {
+                    rez = j;
+                    break;
+                }
+                j++;
+            }
+            return rez != -1 ? rez : array.Length;
+        }
+    }
+}
