a34a3de032ab9676b0a554a35f76ce2c40c8aee6
leonid <leokom96@gmail.com>
2022-06-18 22:20:33 +0200

    Add 283 move zeroes

--- src/Easy.Solutions/Src/Solutions/MoveZeroes_283.cs
+++ src/Easy.Solutions/Src/Solutions/MoveZeroes_283.cs
@@ -2,9 +2,31 @@
 {
     public class MoveZeroes_283
     {
+        /// <summary>
+        /// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+        // Note that you must do this in-place without making a copy of the array.
+        /// </summary>
+
         public void MoveZeroes(int[] nums)
         {
+            for (var i = nums.Length - 1; i > -1; i--)
+            {
+                if (nums[i] != 0) continue;
 
+                // shift
+                var j = i;
+                while (j < nums.Length - 1)
+                {
+                    if (nums[j + 1] != 0)
+                    {
+                        var temp = nums[j + 1];
+                        nums[j + 1] = 0;
+                        nums[j] = temp;
+                        j++;
+                    }
+                    else break;
+                }
+            }
         }
     }
 }
