7d2ad2652fd2175f69010d216223a3e65b9f6089
leonid <leokom96@gmail.com>
2022-06-20 19:21:06 +0200

    Add 896 solution

---
+++ src/Easy.Solutions/Src/Solutions/MonotonicArray_896.cs
@@ -0,0 +1,29 @@
+﻿namespace Easy.Solutions.Solutions
+{
+    public class MonotonicArray_896
+    {
+        public bool IsMonotonic(int[] nums)
+        {
+            bool? isIncrease = null;
+
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1] && isIncrease == null) continue;
+
+                if (nums[i] <= nums[i + 1] && isIncrease == null)
+                {
+                    isIncrease = true;
+                }
+                if (nums[i] >= nums[i + 1] && isIncrease == null)
+                {
+                    isIncrease = false;
+                }
+
+                if (nums[i] > nums[i + 1] && isIncrease.Value) return false;
+
+                if (nums[i] < nums[i + 1] && !isIncrease.Value) return false;
+            }
+            return true;
+        }
+    }
+}
